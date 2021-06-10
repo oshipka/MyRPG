003ea8ca4779cc954b02557feabea06aab73385c
oshipka <olenkashipka@gmail.com>
2021-06-10 15:53:59 +0300

    fixed misc

---
+++ Intermediate/ProjectFiles/ItemProperties.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+class ItemProperties
+{
+public:
+
+	int itemId;
+	std::string itemName;
+	double itemWeight;
+	int itemtype;
+};
+
---
+++ Source/MyRPG/InventoryItemClass.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InventoryItemClass.h"
+
+InventoryItemClass::InventoryItemClass()
+{
+}
+
+InventoryItemClass::~InventoryItemClass()
+{
+}
